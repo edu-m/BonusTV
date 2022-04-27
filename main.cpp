52fb377c3896349e41a1ddcbd4979bec3e05f737
edu-m <MLEDRD02E15C351O@studium.unict.it>
2022-04-27 11:55:46 +0200

    lots of various minor optimizations and fixes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "tvlist.h"
 #include <cstring>
 #include <fstream>
+#include <algorithm>
 #include <sys/stat.h>
 #define EXT ".txt"
 
@@ -9,25 +10,28 @@ using tvlist = List<TV>;
 using namespace std;
 int parseDisplaySize(string);
 
+void insertEntry(Node<TV> *app, ofstream &myfile, bool *found)
+{
+    string uppercaseBrandString = app->getValue().getBrand();
+    transform(uppercaseBrandString.begin(), uppercaseBrandString.end(), uppercaseBrandString.begin(), ::toupper);
+    myfile << "Marca: " << uppercaseBrandString << ", EAN: " << app->getValue().getEAN() << ", Modello: " << app->getValue().getModel() << endl;
+    if (!*found)
+        *found = true;
+}
+
 void pullDataFromMaster(ofstream &myfile, List<int> *tvSizes, unsigned short n, tvlist &odata)
 {
     bool found;
     for (Node<int> *i = tvSizes->getHead(); i != NULL; i = i->getNext())
     {
-        myfile << i->getValue() << " POLLICI" << endl;
+        myfile << i->getValue() << " POLLICI\n";
         found = false;
         for (Node<TV> *app = odata.getHead(); app != NULL; app = app->getNext())
-        {
             if (parseDisplaySize(app->getValue().getModel()) == i->getValue())
-            {
-                myfile << "Marca: " << app->getValue().getBrand() << ", EAN: " << app->getValue().getEAN() << ", Modello: " << app->getValue().getModel() << endl;
-                if (!found)
-                    found = !found;
-            }
-        }
+                insertEntry(app, myfile, &found);
+        myfile << "\n";
         if (!found)
-            myfile << "\t\t**NESSUN MODELLO TROVATO**" << endl;
-        myfile << endl;
+            myfile << "\t\t**NESSUN MODELLO TROVATO**\n\n";
     }
 }
 
@@ -46,13 +50,11 @@ void writeToFile(string file, tvlist &odata)
     List<int> tvSizes;
 
     cout << "Inserisci dimensione (0 per fermarsi): " << endl;
-    while (1)
+    while (i != 0)
     {
         cout << "Inserisci dimensione: ";
         cin >> i;
-        if (i == 0)
-            break;
-        if (!tvSizes.isNodePresent(i))
+        if (!tvSizes.isNodePresent(i) && i != 0)
             tvSizes.insert(i);
     }
     if (tvSizes.getLength() == 0)
@@ -82,9 +84,9 @@ int parseDisplaySize(string str)
     char arr[14]; // max length of SN is 14
 
     strcpy(arr, str.c_str());
-    if ((uint_fast16_t(arr[0]) <= 57 && uint_fast16_t(arr[0]) >= 48))
+    if ((uint_fast8_t(arr[0]) <= 57 && uint_fast8_t(arr[0]) >= 48))
         return stoi(str.substr(0, 2));
-    if ((uint_fast16_t(arr[2]) <= 57 && uint_fast16_t(arr[2]) >= 48))
+    if ((uint_fast8_t(arr[2]) <= 57 && uint_fast8_t(arr[2]) >= 48))
         return stoi(str.substr(2, 2));
     return -1; // errsize
 }
@@ -97,9 +99,9 @@ inline bool fileExists(const string &name)
 
 string sanitizeFileName(string str) // filename must contain ".txt"
 {
-    if (str.find(".txt") != string::npos)
+    if (str.find(EXT) != string::npos)
         return str;
-    return str + ".txt";
+    return str + EXT;
 }
 
 int main()
@@ -111,7 +113,7 @@ int main()
         cout << "Inserisci nome file da ordinare: ";
         cin >> file;
         if (!fileExists(sanitizeFileName(file)))
-        cerr << "File non trovato: \'" << sanitizeFileName(file) << "\'\n";
+            cerr << "File non trovato: \'" << sanitizeFileName(file) << "\'\n";
     } while (!fileExists(sanitizeFileName(file)));
     loadFile(sanitizeFileName(file), masterTVList);
 }
